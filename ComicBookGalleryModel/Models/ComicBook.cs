a545f42a8eb1a483d22ea1853b5eaf914fabc80b
pr0t <prot@prot.com.pl>
2019-06-21 21:10:38 +0200

    Initial commit.

---
+++ ComicBookGalleryModel/Models/ComicBook.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComicBookGalleryModel.Models
+{
+    public class ComicBook
+    {
+        // ID, ComicBookId, ComicBookID - proper names for the primary key column
+        public int Id { get; set; }
+        public string SeriesTitle { get; set; }
+        public int IssueNumber { get; set; }
+        public string Description { get; set; }
+        public DateTime PublishedOn { get; set; }
+    }
+}
