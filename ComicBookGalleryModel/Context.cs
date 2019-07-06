8539baa9b435fd74b5a6cebce3353c1b185f852d
pr0t <prot@prot.com.pl>
2019-07-06 16:28:02 +0200

    ComicBook and Context update.

--- ComicBookGalleryModel/Context.cs
+++ ComicBookGalleryModel/Context.cs
@@ -11,6 +11,13 @@ namespace ComicBookGalleryModel
 
         //}
 
+        public Context()
+        {
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<Context>());
+            //Database.SetInitializer(new CreateDatabaseIfNotExists<Context>());
+            //Database.SetInitializer(new DropCreateDatabaseAlways<Context>());
+        }
+
         public DbSet<ComicBook> ComicBooks { get; set; }
     }
 }
--- ComicBookGalleryModel/Models/ComicBook.cs
+++ ComicBookGalleryModel/Models/ComicBook.cs
@@ -10,5 +10,6 @@ namespace ComicBookGalleryModel.Models
         public int IssueNumber { get; set; }
         public string Description { get; set; }
         public DateTime PublishedOn { get; set; }
+        public decimal? AverageRating { get; set; }
     }
 }
